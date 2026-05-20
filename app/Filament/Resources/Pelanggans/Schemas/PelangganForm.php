0584f001052b4ab5d5175ce61c20ab72ebe1b522
MuhammadRidhoSyp <mridhosyp@gmail.com>
2026-05-20 13:22:10 +0700

    feat: adding customer CRUD

---
+++ app/Filament/Resources/Pelanggans/Schemas/PelangganForm.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Filament\Resources\Pelanggans\Schemas;
+
+use Filament\Schemas\Schema;
+
+class PelangganForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                //
+            ]);
+    }
+}
