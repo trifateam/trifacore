d600b0367dc95289ea7af0435a3e2f7d52707390
mhdghalibsyabandi <mhdghalibs@gmail.com>
2026-06-02 12:10:53 +0700

    feat: instalasi dan konfigurasi seluruh dependencies project

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
-//
+// Alpine.js + Collapse plugin
+import Alpine from 'alpinejs';
+import collapse from '@alpinejs/collapse';
+Alpine.plugin(collapse);
+window.Alpine = Alpine;
+Alpine.start();
+
+// Chart.js
+import Chart from 'chart.js/auto';
+window.Chart = Chart;
+
+// SweetAlert2
+import Swal from 'sweetalert2';
+window.Swal = Swal;
